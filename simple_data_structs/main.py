8c99c817833993e3a35bf04ee3b817b94ee9eda5
Nick Alefanti <alefantinicholas513@gmail.com>
2025-08-13 15:21:38 -0400

    Messed around with lists a bit using methods and mixing in a tuple to see how list methods would be affected by it

--- simple_data_structs/main.py
+++ simple_data_structs/main.py
@@ -1,13 +1,26 @@
 fruits = ["apple", "orange", "banana", "coconut"]
+veggies = ("carrot", "broccoli", "potato")
 
+
+print(len(fruits))
+print("apple" in fruits) # Boolean result
+print(fruits.index("apple")) # int result
+fruits[1] = "pineapple" # reassignment
 print(fruits)
-print(fruits[0])
-print(fruits[:4])
-print(fruits[::-1])
 
-for i in fruits:
-    print(i)
-    for j in fruits:
-        print(j, end=" ")
-    print()
-    
+fruits.append("strawberry")
+print(fruits)
+fruits.remove("banana")
+print(fruits)
+fruits.insert(2, "potato")
+print(fruits)
+fruits.extend(veggies) # So this actually goes through an iterable and adds them to the list.
+print(fruits)
+fruits.insert(len(fruits), veggies) # This just shoves the tuple into the list.
+print(fruits)
+fruits.append(veggies) # does the same thing as insert.
+print(fruits)
+fruits.remove(veggies)
+fruits.remove(veggies)
+fruits.sort() # does not work with things like tuples only strings
+print(fruits)
