6555b8514f372d91b1d361be1083295899ca393c
Nick Alefanti <alefantinicholas513@gmail.com>
2025-08-13 15:47:11 -0400

    Had to resave

--- simple_data_structs/main.py
+++ simple_data_structs/main.py
@@ -1,7 +1,7 @@
 fruits = ["apple", "orange", "banana", "coconut"] # list
+roots = {"turnips", "beetroot", "parsnips"} # set. Note: set does not print out both 1 and True, seems to consider them the same. Seems to also print whichever comes first in the sequence and removes the other.
 veggies = ("carrot", "broccoli", "potato") # tuple
-roots = {"turnips", "beetroot", True, 1, "parsnips"} # set. Note: set does not print out both 1 and True, seems to consider them the same. Seems to also print whichever comes first in the sequence and removes the other.
 
 print(fruits)
-print(veggies)
 print(roots)
+print(veggies)
