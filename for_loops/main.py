1ec7c8ff77ca6f6d5bf3113cb2f3909a75b41fa9
Nick Alefanti <alefantinicholas513@gmail.com>
2025-08-13 14:33:22 -0400

    Did a little work to understand very basics of for loops / nested for loops just using range

--- for_loops/main.py
+++ for_loops/main.py
@@ -1,9 +1,25 @@
 import random
 
-loop_end = [1, 2, 3, 4, 5]
+list_one = [1, 2, 3, 4, 5]
+count = len(list_one)
+print(count)
 
 
-for index in loop_end:
+for index in range(count):
     print(index)
-    print(loop_end)
-    loop_end.append(random.randrange(1, 10))
+    print(list_one)
+    list_one.append(random.randrange(1, 10))
+    print(f"Added {list_one[-1]}")
+    
+list_one.sort()
+print(f"Sorted list: {list_one}")
+
+list_two = [1, 2, 3, 4, 5]
+count = len(list_two)
+print(count)
+
+for index in range(count):
+    print(f"index: {index}")
+    for index2 in range(count):
+        print(f"nested index: {index}")
+        print(f"nested index2: {index2}")
